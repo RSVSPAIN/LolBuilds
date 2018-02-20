51ebaeecbe200e3ccdfccbd719b894a2f0d993fe
amamchaves <amartinez@elpuig.xeill.net>
2018-02-20 20:51:51 +0100

    Comentario de ayuda

--- app/src/main/java/com/example/raul/lolbuilds/HelpActivity.java
+++ app/src/main/java/com/example/raul/lolbuilds/HelpActivity.java
@@ -14,6 +14,7 @@ import android.view.MenuItem;
 public class HelpActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    // AYUDA
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
