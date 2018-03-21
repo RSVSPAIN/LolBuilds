8fd470be38a1ece51d32322c1ca5fb895428a5ed
RSVSPAIN <rsaavedra@elpuig.xeill.net>
2018-03-21 15:26:59 +0100

    Probando Video

--- app/src/main/java/com/example/raul/lolbuilds/VideoActivity.java
+++ app/src/main/java/com/example/raul/lolbuilds/VideoActivity.java
@@ -2,8 +2,11 @@ package com.example.raul.lolbuilds;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,11 +16,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
-public class VideoActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class VideoActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private MenuItem mSearchAction;
+    private boolean isSearchOpened = false;
+    private EditText edtSeach;
+    RecyclerView recyclerView;
+    FirebaseRecyclerAdapter mAdapter;
+    DatabaseReference mReference;
+    String searchReference = "champs/all-champs";
+    String searchTerm = "";
+
+    String preferences_name = "isFirstTime";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +56,8 @@ public class VideoActivity extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        setAdapter();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
@@ -83,5 +110,31 @@ public class VideoActivity extends AppCompatActivity
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    void setAdapter(){
+        Query videosQuery = FirebaseDatabase.getInstance().getReference().child(searchReference);
+
+        FirebaseRecyclerOptions options = new FirebaseRecyclerOptions.Builder<Video>()
+                .setQuery(videosQuery, Video.class)
+                .setLifecycleOwner(this)
+                .build();
+
+        mAdapter = new FirebaseRecyclerAdapter<Video, VideoViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull VideoViewHolder holder, int position, @NonNull Video video) {
+                holder.titulo.setText(video.titulo);
+                Glide.with(VideoActivity.this).load(video.videoName).into(holder.video);
+            }
+
+            @Override
+            public VideoViewHolder onCreateViewHolder(ViewGroup parent, int i) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_video, parent, false);
+                return new VideoViewHolder(view);
+            }
+
+        };
+
+        recyclerView.setAdapter(mAdapter);
     }
 }
